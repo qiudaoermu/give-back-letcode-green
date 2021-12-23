6b6587bc50eb6ce90a11ed4c05e185800b0365ef
chenzhenfei <793385821@qq.com>
2021-12-23 10:27:05 +0800

    feat: 简书图片样式修改

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
-import Vue from 'vue';
-import App from './app.vue';
-import './assets/styles/global.styl'
+import Vue from "vue";
+import App from "./app.vue";
+import "./assets/styles/global.styl";
 
 // const root = document.createElement('div');
 // document.body.appendChild(root);
 import $ from "jquery";
-$('.col-xs-7.col-xs-offset-1.aside').css({ display: 'none' })
+$(".col-xs-7.col-xs-offset-1.aside").css({ display: "none" });
+// quora
+$("#mainContent").css({ with: 1080 });
 
 // new Vue({
 //     render: (h) => h(App)
 // }).$mount(root);
+// 简书
+$(".image-container").css({ with: "100%" });
+// 掘金
+// 可以使用第三方图片 如简书
+let referrerDom = '<meta name="referrer" content="no-referrer" />';
+$("body").append(referrerDom);
 
+$(document).ready(function() {
+  //code 简书自动展开阅读全文
+  $(".ant-btn").click();
+});
